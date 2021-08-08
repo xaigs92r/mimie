87d978f5ed3871bd2830c7d47498ed704004ccaa
chaowen guo <chaowen.guo1@gmail.com>
2021-08-08 00:38:03 -0500

    Update ha.js

--- ha.js
+++ ha.js
@@ -4,6 +4,7 @@ const browser = await chromium.launch({executablePath:'/usr/bin/google-chrome', 
 const context = await browser.newContext()
 let alexamaster = await context.newPage()
 await alexamaster.goto('https://cashmining.me/')
+await alexamaster.click('a.nav-link')
 await alexamaster.waitForTimeout(1000 * 60 * 2)
 await alexamaster.screenshot({path:'ha.png'})
 await browser.close()
