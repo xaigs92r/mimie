51a2ad10e590d90160096c8a1e6bbc267cb39c7b
chaowen guo <chaowen.guo1@gmail.com>
2021-09-14 01:34:24 -0500

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,1 +1,8 @@
+import {chromium} from 'playwright-chromium'
 
+const browser = await chromium.launch({channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false})
+const context = await browser.newContext({recordVideo:{dir:'videos'}})
+const alexamaster = await context.newPage()
+await alexamaster.goto('https://www.ytpals.com/login/final/UCkKr6PX7hPxw0E7vYXeDbvg/')
+await alexamaster.waitForTimeout(1000 * 60 * 2)
+await browser.close()
