f9bb497da55f67ec75a8b2c780b5f53af551d771
chaowen guo <chaowen.guo1@gmail.com>
2021-09-15 05:34:27 -0500

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,13 +1,9 @@
 import {chromium} from 'playwright-chromium'
 
-const browser = await chromium.launch({channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false})
+const browser = await chromium.launchPersistentContext('google-chrome', {channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false})
 const context = await browser.newContext({recordVideo:{dir:'videos'}})
 const alexamaster = await context.newPage()
-await alexamaster.goto('https://accounts.google.com/')
-await alexamaster.fill('input[type="email"]', 'chaowen.guo1@gmail.com')
-await alexamaster.click('div#identifierNext')
-await alexamaster.waitForNavigation()
-await alexamaster.fill('input[type="password"]', 'HL798820y+')
+await alexamaster.goto('https://www.youtube.com/watch?v=VZ9Fo6SWJs0')
 //await alexamaster.click('div#passwordNext')
 /*await alexamaster.fill('input[name="password"]', 'HL798820y+')
 await alexamaster.click('button[type="submit"]')
