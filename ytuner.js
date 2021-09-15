1c14fa9626b081f00a90925b084cda9ce56bd581
chaowen guo <chaowen.guo1@gmail.com>
2021-09-15 01:14:58 -0500

    Update ytuner.js

--- ytuner.js
+++ ytuner.js
@@ -50,7 +50,11 @@ while (!globalThis.Object.is(await ytuner.url(), 'https://www.ytuner.com/dashboa
     }
     await select.selectOption(option[0])
     await ytuner.waitForSelector('input#form-agree').then(_ => _.evaluateHandle(_ => _.click()))
-    while (!globalThis.Object.is(await moviePlayer.evaluateHandle(_ => _.getPlayerState()).then(_ => _.jsonValue()), 0)) await youtube.waitForTimeout(1000 * 60)
+    while (!globalThis.Object.is(await moviePlayer.evaluateHandle(_ => _.getPlayerState()).then(_ => _.jsonValue()), 0))
+    {
+        await youtube.waitForTimeout(1000 * 60)
+	console.log(await moviePlayer.evaluateHandle(_ => _.getPlayerState()).then(_ => _.jsonValue()))
+    }
     await youtube.close()
     await ytuner.click('a#submit')
 }
