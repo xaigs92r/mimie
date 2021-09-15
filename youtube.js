b6cc939339e9e84d4ac1f5407e44e8975bf6b8a2
chaowen guo <chaowen.guo1@gmail.com>
2021-09-15 03:59:46 -0500

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -3,8 +3,8 @@ import {chromium} from 'playwright-chromium'
 const browser = await chromium.launch({channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false})
 const context = await browser.newContext({recordVideo:{dir:'videos'}})
 const alexamaster = await context.newPage()
-await alexamaster.goto('https://www.ytpals.com/login/final/UCkKr6PX7hPxw0E7vYXeDbvg/')
-await alexamaster.fill('input[name="password"]', 'HL798820y+')
+await alexamaster.goto('https://accounts.google.com/')
+/*await alexamaster.fill('input[name="password"]', 'HL798820y+')
 await alexamaster.click('button[type="submit"]')
 await alexamaster.waitForLoadState()
 await alexamaster.evaluateHandle(() => globalThis.document.starter.submit())
@@ -16,6 +16,6 @@ await alexamaster.evaluateHandle(() => globalThis.document.starter.submit())
     await alexamaster.waitForTimeout(1000 * 60)
     await alexamaster.click('a[onclick^="confirmAll"]')
     await alexamaster.waitForLoadState()
-//}
+//}*/
 await alexamaster.waitForTimeout(1000 * 60 * 2)
 await browser.close()
