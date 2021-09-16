0dbd73fe6449dfa86f21cc867e6ad3a71f0432d6
chaowen guo <chaowen.guo1@gmail.com>
2021-09-15 23:22:13 -0500

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,20 +1,24 @@
 import {chromium} from 'playwright-chromium'
 import process from 'process'
 
-const context = await chromium.launchPersistentContext('google-chrome', {channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false, recordVideo:{dir:'videos'}})
-const alexamaster = await context.newPage()
-await alexamaster.goto('https://www.ytpals.com/login/final/UCkKr6PX7hPxw0E7vYXeDbvg/')
-await alexamaster.fill('input[name="password"]', process.argv.at(2))
-await alexamaster.click('button[type="submit"]')
-await alexamaster.waitForNavigation()
-await alexamaster.evaluateHandle(() => globalThis.document.starter.submit())
-for (const _ of globalThis.Array(20).keys())
+async function main(_)
 {
-    const [popup] = await globalThis.Promise.all([alexamaster.waitForEvent('popup'), alexamaster.click('a[onclick^="openWin"]')])
-    await popup.click('yt-formatted-string.ytd-subscribe-button-renderer')
-    await alexamaster.waitForTimeout(1000 * 40)
-    await popup.close()
-    await alexamaster.click('a[onclick^="confirmAll"]')
+    const context = await chromium.launchPersistentContext('google-chrome', {channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false, recordVideo:{dir:'videos'}})
+    const alexamaster = await context.newPage()
+    await alexamaster.goto(`https://www.${_}.com/login/final/UCkKr6PX7hPxw0E7vYXeDbvg/`)
+    await alexamaster.fill('input[name="password"]', process.argv.at(2))
+    await alexamaster.click('button[type="submit"]')
     await alexamaster.waitForNavigation()
+    await alexamaster.evaluateHandle(() => globalThis.document.starter.submit())
+    for (const _ of globalThis.Array(20).keys())
+    {
+        const [popup] = await globalThis.Promise.all([alexamaster.waitForEvent('popup'), alexamaster.click('a[onclick^="openWin"]')])
+        await popup.click('yt-formatted-string.ytd-subscribe-button-renderer')
+        await alexamaster.waitForTimeout(1000 * 40)
+        await popup.close()
+        await alexamaster.click('a[onclick^="confirmAll"]')
+        await alexamaster.waitForNavigation()
+    }
+    await context.close()
 }
-await context.close()
+await globalThis.Promise.all(['sonuker', 'subpals'].map(main))
