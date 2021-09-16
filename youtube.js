89ad942e8431bd1cafbfbc5431e91924b26409d8
chaowen guo <chaowen.guo1@gmail.com>
2021-09-15 21:13:47 -0500

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -7,13 +7,13 @@ await alexamaster.fill('input[name="password"]', 'HL798820y+')
 await alexamaster.click('button[type="submit"]')
 await alexamaster.waitForNavigation()
 await alexamaster.evaluateHandle(() => globalThis.document.starter.submit())
-for (const _ of globalThis.Array(2).keys())
-{
+//for (const _ of globalThis.Array(2).keys())
+//{
     const [popup] = await globalThis.Promise.all([alexamaster.waitForEvent('popup'), alexamaster.click('a[onclick^="openWin"]')])
     await popup.click('yt-formatted-string.ytd-subscribe-button-renderer')
     await alexamaster.waitForTimeout(1000 * 40)
     await alexamaster.click('a[onclick^="confirmAll"]')
-    await alexamaster.waitForNavigation()
-}
+    //await alexamaster.waitForNavigation()
+//}
 await alexamaster.waitForTimeout(1000 * 60)
 await context.close()
