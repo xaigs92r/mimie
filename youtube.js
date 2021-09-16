b996e9d9913ab66d2379216017601e18cba9e516
chaowen guo <chaowen.guo1@gmail.com>
2021-09-15 21:42:51 -0500

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -14,7 +14,6 @@ for (const _ of globalThis.Array(2).keys())
     await alexamaster.waitForTimeout(1000 * 40)
     await popup.close()
     await alexamaster.click('a[onclick^="confirmAll"]')
-    await alexamaster.waitForTimeout(1000 * 60)
+    await alexamaster.waitForNavigation()
 }
-await alexamaster.waitForTimeout(1000 * 60)
 await context.close()
