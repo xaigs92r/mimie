8c4b63c319742cd89dd07368efd0af8d0a6edc7a
chaowen guo <chaowen.guo1@gmail.com>
2021-10-28 23:18:05 -0500

    Update and rename youtube.js to subscribe.js

---
+++ subscribe.js
@@ -0,0 +1,27 @@
+import {chromium} from 'playwright-chromium'
+import process from 'process'
+
+const context = await chromium.launchPersistentContext('google-chrome', {channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false, recordVideo:{dir:'videos'}})
+const subscribe = await context.newPage()
+for (const _ of ['sonuker', 'subpals', 'ytpals'])
+{     
+    await subscribe.goto(`https://www.${_}.com/login/final/UCkKr6PX7hPxw0E7vYXeDbvg/`)
+    await subscribe.fill('input[name="password"]', process.argv.at(2))
+    await subscribe.click('button[type="submit"]')
+    await subscribe.waitForNavigation()
+    await subscribe.evaluateHandle(() => globalThis.document.starter.submit())
+    for (const _ of globalThis.Array(20).keys())
+    {
+        const [popup] = await globalThis.Promise.all([subscribe.waitForEvent('popup'), subscribe.click('a[onclick^="openWin"]')])
+        try
+        {
+            await popup.click('yt-formatted-string.ytd-subscribe-button-renderer')
+        }
+        catch {}
+        await subscribe.waitForTimeout(1000 * 40)
+        await popup.close()
+        await subscribe.click('a[onclick^="confirmAll"]')
+        await subscribe.waitForNavigation()
+    }
+}
+await context.close()
