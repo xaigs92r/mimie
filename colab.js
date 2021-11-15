c37a344096b7385e56f1a692bb5e73292ad2ac6a
chaowen guo <chaowen.guo1@gmail.com>
2021-11-15 14:45:44 -0600

    Update colab.js

--- colab.js
+++ colab.js
@@ -4,7 +4,7 @@ const context = await chromium.launchPersistentContext('google-chrome', {channel
 const colab = await context.newPage()
 await colab.goto('https://colab.research.google.com/github/chaowenGUOorg/pal/blob/main/colab.ipynb')
 await colab.keyboard.press('Control+F9')
-await colab.click('#ok')
+//await colab.click('#ok')
 await colab.waitForTimeout(60)
 await colab.screenshot({path:'screenshot.png'})
 await context.close()
