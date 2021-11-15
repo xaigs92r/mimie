4a3e74b82cc70c60a4a4e60b7ca963c201ef3338
chaowen guo <chaowen.guo1@gmail.com>
2021-11-15 14:37:21 -0600

    Update colab.js

--- colab.js
+++ colab.js
@@ -1,6 +1,6 @@
 import {chromium} from 'playwright-chromium'
 
-const context = await chromium.launchPersistentContext('google-chrome', {channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false, recordVideo:{dir:'videos'}})
+const context = await chromium.launchPersistentContext('google-chrome', {channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false})
 const colab = await context.newPage()
 await colab.goto('https://colab.research.google.com/github/chaowenGUOorg/surf/blob/main/colab.ipynb')
 await colab.click('iron-icon#icon')
