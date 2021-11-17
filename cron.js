eb6a7e9caef9bfb3765e81e884035481e0c6778f
chaowen guo <chaowen.guo1@gmail.com>
2021-11-17 02:03:17 -0600

    Update cron.js

--- cron.js
+++ cron.js
@@ -7,6 +7,6 @@ client.scopes = ['https://www.googleapis.com/auth/cloud-platform']
 await client.request({url:'https://dns.googleapis.com/dns/v1/projects/chaowenguo'})
 console.log(client.credentials.access_token)
 
-job = 'https://cloudscheduler.googleapis.com/v1/projects/chaowenguo/locations/us-central1/jobs'
+const job = 'https://cloudscheduler.googleapis.com/v1/projects/chaowenguo/locations/us-central1/jobs'
 const response = await fetch(job + '/short', {headers:{'authorization':`Bearer ${client.credentials.access_token}`}})
 console.log(response.status)
