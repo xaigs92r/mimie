266abeb7c4f7507c577e3413b2c76fe32fd35f85
chaowen guo <chaowen.guo1@gmail.com>
2021-11-17 02:24:09 -0600

    Update cron.js

--- cron.js
+++ cron.js
@@ -1,12 +1,14 @@
 import {promises as fs} from 'fs'
 import {auth} from 'google-auth-library'
 import fetch from 'node-fetch'
+import url from 'url'
+import process from 'process'
 
 const client = auth.fromJSON(globalThis.JSON.parse(await fs.readFile('gcloud')))
 client.scopes = ['https://www.googleapis.com/auth/cloud-platform']
 await client.request({url:'https://dns.googleapis.com/dns/v1/projects/chaowenguo'})
-console.log(client.credentials.access_token)
 
 const job = 'https://cloudscheduler.googleapis.com/v1/projects/chaowenguo/locations/us-central1/jobs'
-const response = await fetch(job + '/short', {headers:{'authorization':`Bearer ${client.credentials.access_token}`}})
-console.log(response.status)
+const response = await fetch(job + 'colab', {headers:{'authorization':`Bearer ${client.credentials.access_token}`}})
+if (globalThis.Object.is(response.status, 200)) await fetch(job + 'colab', {method:'DELETE', headers:{'authorization':`Bearer ${client.credentials.access_token}`}})
+await fetch(job, {method:'POST', headers:{'authorization':`Bearer ${client.credentials.token}`}, body:globalThis.JSON.stringify({name:url.parse(job).pathname, schedule:'*/10 * * * *', httpTarget:{uri:`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/dispatches`, headers:{Authorization:`token ${process.argv[2]}`}, body:base64.b64encode(globalThis.JSON.stringify({'event_type':repository}).encode()).decode()}})
