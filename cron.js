234cb9b0a87a56c2a2cda7bf8df379f4c7e938bd
chaowen guo <chaowen.guo1@gmail.com>
2021-11-17 01:34:03 -0600

    Update cron.js

--- cron.js
+++ cron.js
@@ -5,5 +5,4 @@ const keys = globalThis.JSON.parse(await fs.readFile('gcloud'))
 const client = new JWT({email: keys.client_email, key: keys.private_key, scopes: ['https://www.googleapis.com/auth/cloud-platform']})
 const url = 'https://dns.googleapis.com/dns/v1/projects/chaowenguo'
 const res = await client.request({url})
-console.log(res.data)
 console.log(client.credentials.access_token)
