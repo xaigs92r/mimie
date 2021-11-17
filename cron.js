45489378510f7284f466a6040e018c9f9e4e661e
chaowen guo <chaowen.guo1@gmail.com>
2021-11-17 02:02:32 -0600

    Update cron.js

--- cron.js
+++ cron.js
@@ -10,8 +10,3 @@ console.log(client.credentials.access_token)
 job = 'https://cloudscheduler.googleapis.com/v1/projects/chaowenguo/locations/us-central1/jobs'
 const response = await fetch(job + '/short', {headers:{'authorization':`Bearer ${client.credentials.access_token}`}})
 console.log(response.status)
-        /*async with session.get('/'.join((job, repository)), headers={'authorization':f'Bearer {credentials.token}'}) as response:
-            if response.status == 200:
-                async with session.delete('/'.join((job, repository)), headers={'authorization':f'Bearer {credentials.token}'}) as _: pass
-        async with session.post(job, headers={'authorization':f'Bearer {credentials.token}'}, json={'name':'/'.join((urllib.parse.urlparse(job).path.split('/', 2)[-1], repository)), 'schedule':'*/10 * * * *', 'httpTarget':{'uri':f'https://api.github.com/repos/{os.getenv("GITHUB_REPOSITORY")}/dispatches', 'headers':{'Authorization':f'token {parser.parse_args().github}'}, 'body':base64.b64encode(json.dumps({'event_type':repository}).encode()).decode()}}) as _: pass
-        */
