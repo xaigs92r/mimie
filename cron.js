6e3d86456168ae7388569425ce35a3730baf2c42
chaowen guo <chaowen.guo1@gmail.com>
2021-11-17 01:21:42 -0600

    Update cron.js

--- cron.js
+++ cron.js
@@ -1,3 +1,6 @@
 import {promises as fs} from 'fs'
+import {auth} from 'google-auth-library'
 
-console.log(globalThis.JSON.parse(await fs.readFile('gcloud')))
+const client = auth.fromJSON(globalThis.JSON.parse(await fs.readFile('gcloud')))
+client.scopes = ['https://www.googleapis.com/auth/cloud-platform']
+console.log(client.credentials.access_token)
