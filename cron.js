edc7613434a466078bb91c8419e17aafd22cb91e
chaowen guo <chaowen.guo1@gmail.com>
2021-11-17 17:54:53 -0600

    Update cron.js

--- cron.js
+++ cron.js
@@ -9,5 +9,5 @@ await client.request({url:'https://dns.googleapis.com/dns/v1/projects/chaowenguo
 
 const job = 'https://cloudscheduler.googleapis.com/v1/projects/chaowenguo/locations/us-central1/jobs'
 const response = await fetch(job + '/colab', {headers:{'authorization':`Bearer ${client.credentials.access_token}`}})
-if (response.ok) await fetch(job + '/colab', {method:'DELETE', headers:{'authorization':`Bearer ${client.credentials.access_token}`}})
-await fetch(job, {method:'POST', headers:{'authorization':`Bearer ${client.credentials.access_token}`}, body:globalThis.JSON.stringify({name:new globalThis.URL(job).pathname.split('/').slice(2).join('/') + '/colab', schedule:'*/10 * * * *', httpTarget:{uri:`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/dispatches`, headers:{Authorization:`token ${process.argv.at(2)}`}, body:globalThis.btoa(globalThis.JSON.stringify({'event_type':'colab'}))}})})
+if (response.ok) await fetch(job + '/colab', {method:'delete', headers:{'authorization':`Bearer ${client.credentials.access_token}`}})
+await fetch(job, {method:'post', headers:{'authorization':`Bearer ${client.credentials.access_token}`}, body:globalThis.JSON.stringify({name:new globalThis.URL(job).pathname.split('/').slice(2).join('/') + '/colab', schedule:'*/10 * * * *', httpTarget:{uri:`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/dispatches`, headers:{Authorization:`token ${process.argv.at(2)}`}, body:globalThis.btoa(globalThis.JSON.stringify({'event_type':'colab'}))}})})
