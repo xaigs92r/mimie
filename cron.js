b4b38383bff4d89ec29a309d4299ef47146f9eca
chaowen guo <chaowen.guo1@gmail.com>
2021-11-17 01:59:34 -0600

    Update cron.js

--- cron.js
+++ cron.js
@@ -8,7 +8,7 @@ await client.request({url:'https://dns.googleapis.com/dns/v1/projects/chaowenguo
 console.log(client.credentials.access_token)
 
 job = 'https://cloudscheduler.googleapis.com/v1/projects/chaowenguo/locations/us-central1/jobs'
-const response = await fetch.get(job + '/short', headers:{'authorization':`Bearer ${client.credentials.access_token}`})
+const response = await fetch(job + '/short', headers:{'authorization':`Bearer ${client.credentials.access_token}`})
 console.log(response.status)
         /*async with session.get('/'.join((job, repository)), headers={'authorization':f'Bearer {credentials.token}'}) as response:
             if response.status == 200:
