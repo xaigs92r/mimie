2f72bab5199de62d060d9dc68bffe8c8326fc84c
chaowen guo <chaowen.guo1@gmail.com>
2021-11-18 17:12:33 -0600

    Update watch.js

--- watch.js
+++ watch.js
@@ -3,7 +3,7 @@ import {chromium} from 'playwright-chromium'
 const browser = await chromium.launch({channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false})
 globalThis.setTimeout(async () => await browser.close(), 1000 * 60 * 60 * 1.7)
 const youtube = await browser.newPage({recordVideo:{dir:'videos'}})
-await youtube.goto('https://www.youtube.com/watch?v=6ph-UsapQmEonk')
+await youtube.goto('https://www.youtube.com/watch?v=in-t65q8u7Q')
 const moviePlayer = await youtube.$('div#movie_player')
 await moviePlayer.evaluateHandle(_ => _.style.display = 'block')
 await moviePlayer.waitForElementState('visible')
