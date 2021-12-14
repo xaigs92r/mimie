a503fe9f4e5062481227235ef67c2289ffb0625e
chaowen guo <chaowen.guo1@gmail.com>
2021-12-14 12:08:04 -0600

    Update ptc.js

--- ptc.js
+++ ptc.js
@@ -3,6 +3,7 @@ import process from 'process'
 import fetch from 'node-fetch'
 import {promises as fs} from 'fs'
 import {Buffer} from 'buffer'
+import tesseract from 'node-tesseract-ocr'
 
 const browser = await chromium.launch({channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false})
 const starClicks = await browser.newPage({recordVideo:{dir:'videos'}})
@@ -10,5 +11,6 @@ await starClicks.goto('https://www.star-clicks.com/login')
 await starClicks.fill('input#Email', 'chaowen.guo1@gmail.com')
 await starClicks.fill('input#Password', process.argv.at(2))
 await fs.writeFile('haha.jpg', Buffer.from(await fetch('https://www.star-clicks.com/' + await starClicks.locator('img#Captcha2_CaptchaImage').getAttribute('src')).then(_ => _.arrayBuffer())), 'binary')
+console.log(await tesseract.recognize('haha.jpg'))
 await starClicks.waitForTimeout(1000 * 60)
 await browser.close()
