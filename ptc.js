b512d8969e7fdae5c8cbe40fc92735dfd0fd3d68
chaowen guo <chaowen.guo1@gmail.com>
2021-12-14 11:28:13 -0600

    Update ptc.js

--- ptc.js
+++ ptc.js
@@ -1,11 +1,13 @@
 import {chromium} from 'playwright-chromium'
 import process from 'process'
+import fetch from 'node-fetch'
+import {promises as fs} from 'fs'
 
 const browser = await chromium.launch({channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false})
 const starClicks = await browser.newPage({recordVideo:{dir:'videos'}})
 await starClicks.goto('https://www.star-clicks.com/login')
 await starClicks.fill('input#Email', 'chaowen.guo1@gmail.com')
 await starClicks.fill('input#Password', process.argv.at(2))
-console.log(await starClicks.locator('img#Captcha2_CaptchaImage').getAttribute('src'))
+await fs.writeFile('haha.png', await fetch('https://www.star-clicks.com/' + await starClicks.locator('img#Captcha2_CaptchaImage').getAttribute('src')).then(_ => _.blob()), 'binary')
 await starClicks.waitForTimeout(1000 * 60)
 await browser.close()
