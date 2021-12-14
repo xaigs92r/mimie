de1f7626f2d838be6e506798def8d4f00a59f2b4
chaowen guo <chaowen.guo1@gmail.com>
2021-12-13 22:50:35 -0600

    Update server.js

--- cron/server.js
+++ cron/server.js
@@ -2,4 +2,5 @@ import http from 'http'
 import process from 'process'
 import fetch from 'node-fetch'
 
-http.createServer((req, res) => res.end(process.env.haha)).listen(80)
+http.createServer((req, res) => res.end(process.env.github)).listen(80)
+await fetch(`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/dispatches`, {method:'post', headers:{authorization:`token ${process.env.github}`}, body:globalThis.JSON.stringify({event_type:'youtube'})})
