5975e177bfa1b73b780fd419cd995c652523528e
chaowen guo <chaowen.guo1@gmail.com>
2021-12-16 08:24:41 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ import org.opencv.core.Scalar;
 class Main
 {
   public static void main(String[] args) {
-    System.load("/usr/lib/jni/lib" + Core.NATIVE_LIBRARY_NAME + ".so");
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     System.out.println("Welcome to OpenCV " + Core.VERSION);
     Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
     System.out.println( "mat = " + mat.dump() );
