7d4b8ce3248c68f4031a462c891b613c0090095d
chaowen guo <chaowen.guo1@gmail.com>
2021-12-16 07:20:27 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,12 +8,7 @@ class Main
   public static void main(String[] args) {
     System.load("/usr/lib/jni/lib" + Core.NATIVE_LIBRARY_NAME + ".so");
     System.out.println("Welcome to OpenCV " + Core.VERSION);
-    Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
-    System.out.println("OpenCV Mat: " + m);
-    Mat mr1 = m.row(1);
-    mr1.setTo(new Scalar(1));
-    Mat mc5 = m.col(5);
-    mc5.setTo(new Scalar(5));
-    System.out.println("OpenCV Mat data:\n" + m.dump());
+    Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
+    System.out.println( "mat = " + mat.dump() );
   }
 }
