fde3fc8cf487f593209f846fbb7c269a9456b91c
chaowen guo <chaowen.guo1@gmail.com>
2021-12-16 17:24:45 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main
 {
     public static void main(final java.lang.String[] args)
     {
-        java.lang.System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        java.lang.System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
         final var mat = org.opencv.core.Mat.eye( 3, 3, org.opencv.core.CvType.CV_8UC1 );
         java.lang.System.out.println(mat.dump());
     }
