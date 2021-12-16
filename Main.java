b8deb99dcf63294fcc55785f0facfe4e123a0d2a
chaowen guo <chaowen.guo1@gmail.com>
2021-12-16 17:48:18 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,15 @@ public class Main
     public static void main(final java.lang.String[] args)
     {
         java.lang.System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
-        try (final var browser = com.microsoft.playwright.chromium().launch())
+        try (final var playwright = com.microsoft.playwright.Playwright.create())
         {
-            final var context = browser.newContext();
-            final var page = context.newPage();
-            page.navigate("http://whatsmyuseragent.org/");
-            page.screenshot(new com.microsoft.playwright.Page.ScreenshotOptions().setPath(java.nio.file.Paths.get("haha.png")));
+            try (final var browser = playwright.chromium().launch())
+            {
+                final var context = browser.newContext();
+                final var page = context.newPage();
+                page.navigate("http://whatsmyuseragent.org/");
+                page.screenshot(new com.microsoft.playwright.Page.ScreenshotOptions().setPath(java.nio.file.Paths.get("haha.png")));
+            }
         }
         final var mat = org.opencv.core.Mat.eye( 3, 3, org.opencv.core.CvType.CV_8UC1 );
         java.lang.System.out.println(mat.dump());
