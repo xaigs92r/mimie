4874b3de5ed9d961a7d165e4dabab4b1deb9af29
chaowen guo <chaowen.guo1@gmail.com>
2021-12-16 17:22:43 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,9 @@
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.CvType;
-import org.opencv.core.Scalar;
-
 public class Main
 {
-  public static void main(String[] args)
-  {
-    System.load(Core.NATIVE_LIBRARY_NAME);
-    Mat mat = new Mat();
-    System.out.println( "mat = " + mat.dump() );
-  }
+    public static void main(final java.lang.String[] args)
+    {
+        java.lang.System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        final var mat = org.opencv.core.Mat.eye( 3, 3, org.opencv.core.CvType.CV_8UC1 );
+        java.lang.System.out.println(mat.dump());
+    }
 }
