a6e8cd6638ec39952feae25148b95a5f9f9cf73f
chaowen guo <chaowen.guo1@gmail.com>
2021-12-17 17:48:51 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,7 +11,8 @@ public class Main
                 final var page = context.newPage();
                 page.navigate("https://www.star-clicks.com/login");
                 final var email = page.locator("input#Email");
-                email.evaluateHandle("_ => _.value = 'chaowen.guo1@gmail.com'");
+                email.evaluateHandle("_ => _.removeAttribute('class')");
+                email.fill("chaowen.guo1@gmail.com");
                 page.fill("input#Password", args[0]);
                 final var mat = org.opencv.imgcodecs.Imgcodecs.imdecode(new org.opencv.core.MatOfByte(java.net.http.HttpClient.newBuilder().build().sendAsync(java.net.http.HttpRequest.newBuilder(java.net.URI.create("https://www.star-clicks.com/" + page.locator("img#Captcha2_CaptchaImage").getAttribute("src"))).build(), java.net.http.HttpResponse.BodyHandlers.ofByteArray()).thenApply(java.net.http.HttpResponse::body).join()), org.opencv.imgcodecs.Imgcodecs.IMREAD_GRAYSCALE);
                 org.opencv.imgproc.Imgproc.threshold(mat, mat, 0, 255, org.opencv.imgproc.Imgproc.THRESH_BINARY + org.opencv.imgproc.Imgproc.THRESH_OTSU);
