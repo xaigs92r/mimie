0d83f4e99037b010f01f4adc4ac42ce517179f2a
chaowen guo <chaowen.guo1@gmail.com>
2021-12-17 15:17:50 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,11 +10,11 @@ public class Main
                 final var context = browser.newContext(new com.microsoft.playwright.Browser.NewContextOptions().setRecordVideoDir(java.nio.file.Paths.get("videos")));
                 final var page = context.newPage();
                 page.navigate("https://www.star-clicks.com/login");
-                java.util.concurrent.TimeUnit.SECONDS.sleep(15);
                 page.fill("input#Email", "chaowen.guo1@gmail.com");
                 //page.fill("input#Password", args[0]);
                 final var mat = org.opencv.imgcodecs.Imgcodecs.imdecode(new org.opencv.core.MatOfByte(java.net.http.HttpClient.newBuilder().build().sendAsync(java.net.http.HttpRequest.newBuilder(java.net.URI.create("https://www.star-clicks.com/" + page.locator("img#Captcha2_CaptchaImage").getAttribute("src"))).build(), java.net.http.HttpResponse.BodyHandlers.ofByteArray()).thenApply(java.net.http.HttpResponse::body).join()), org.opencv.imgcodecs.Imgcodecs.IMREAD_GRAYSCALE);
                 org.opencv.imgproc.Imgproc.threshold(mat, mat, 0, 255, org.opencv.imgproc.Imgproc.THRESH_BINARY + org.opencv.imgproc.Imgproc.THRESH_OTSU);
+                org.opencv.imgproc.Imgproc.morphologyEx(mat, mat, org.opencv.imgproc.Imgproc.MORPH_CLOSE, new org.opencv.core.Mat());
                 org.opencv.imgcodecs.Imgcodecs.imwrite("page.jpg", mat);
                 java.util.concurrent.TimeUnit.SECONDS.sleep(60);
             }
