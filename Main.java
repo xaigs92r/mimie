98a46a7dc67a387cad88bad8cea8282f24973df1
chaowen guo <chaowen.guo1@gmail.com>
2021-12-16 19:13:25 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,11 +12,10 @@ public class Main
                 page.navigate("https://www.star-clicks.com/login");
                 page.fill("input#Email", "chaowen.guo1@gmail.com");
                 //page.fill("input#Password", args[0]);
-                java.nio.file.Files.write(java.nio.file.Paths.get("page.jpg"), java.net.http.HttpClient.newBuilder().build().sendAsync(java.net.http.HttpRequest.newBuilder(java.net.URI.create("https://www.star-clicks.com/" + page.locator("img#Captcha2_CaptchaImage").getAttribute("src"))).build(), java.net.http.HttpResponse.BodyHandlers.ofByteArray()).thenApply(java.net.http.HttpResponse::body).join());
+                final var mat = org.opencv.imgcodecs.Imgcodecs.imdecode(org.opencv.core.MatOfByte.fromArray(java.net.http.HttpClient.newBuilder().build().sendAsync(java.net.http.HttpRequest.newBuilder(java.net.URI.create("https://www.star-clicks.com/" + page.locator("img#Captcha2_CaptchaImage").getAttribute("src"))).build(), java.net.http.HttpResponse.BodyHandlers.ofByteArray()).thenApply(java.net.http.HttpResponse::body).join()), org.opencv.imgcodecs.Imgcodecs.CV_LOAD_IMAGE_GRAYSCALE);
+                org.opencv.imgcodecs.Imgcodecs.imwrite("page.jpg", mat);
                 page.screenshot(new com.microsoft.playwright.Page.ScreenshotOptions().setPath(java.nio.file.Paths.get("haha.jpg")));
             }
         }
-        final var mat = org.opencv.core.Mat.eye( 3, 3, org.opencv.core.CvType.CV_8UC1 );
-        java.lang.System.out.println(mat.dump());
     }
 }
