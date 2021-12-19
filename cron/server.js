8a3d2a639d4ccb390cb9d6d244b6e4fa399112d1
chaowen guo <chaowen.guo1@gmail.com>
2021-12-19 15:35:31 -0600

    Update server.js

--- cron/server.js
+++ cron/server.js
@@ -4,3 +4,4 @@ import fetch from 'node-fetch'
 
 http.createServer((req, res) => res.end('subscribe')).listen(80)
 globalThis.setInterval(async () => await fetch(`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/dispatches`, {method:'post', headers:{authorization:`token ${process.env.github}`}, body:globalThis.JSON.stringify({event_type:'subscribe'})}), 1000 * 60 * 60 * 13)
+globalThis.setInterval(async () => await fetch(`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/dispatches`, {method:'post', headers:{authorization:`token ${process.env.github}`}, body:globalThis.JSON.stringify({event_type:'ptc'})}), 1000 * 60 * 60 * 25)
