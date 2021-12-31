1a41ac04e093866d339e3620d644b2f6ea50e7d4
chaowen guo <chaowen.guo1@gmail.com>
2021-12-30 23:52:49 -0600

    Update digit.py

--- digit.py
+++ digit.py
@@ -14,6 +14,8 @@ async def main():
         await email.fill('chaowen.guo1@gmail.com')        
         await page.fill('input#Password', parser.parse_args().password)
         mat = cv2.imdecode(numpy.frombuffer(await page.locator('img#Captcha2_CaptchaImage').screenshot(), numpy.uint8), 0)
+        mat = cv2.threshold(mat, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        mat = cv2.morphologyEx(mat, cv2.MORPH_CLOSE, None)
         cv2.imwrite('haha.png', mat)
         await page.screenshot(path='hahaha.png')
         await browser.close()
