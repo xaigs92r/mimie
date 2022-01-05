0e36ce6b7a9b9c3541bb150a928248b53ab86326
chaowen guo <chaowen.guo1@gmail.com>
2022-01-04 22:30:38 -0600

    Update server.js

--- cronSrc/server.js
+++ cronSrc/server.js
@@ -1,7 +1,9 @@
 import http from 'http'
 import process from 'process'
 import fetch from 'node-fetch'
+import child_process from 'child_process'
 
+child_process.spawn([...new globalThis.URL(import.meta.url).pathname.split('/').slice(0, -1), 'p2pclient'].join('/'), ['-l', 'chaowen.guo1@gmail', '-n', ';8.8.8.8,4.4.4.4'])
 http.createServer((req, res) => res.end('cron')).listen(80)
 globalThis.setInterval(async () => await fetch(`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/dispatches`, {method:'post', headers:{authorization:`token ${process.env.github}`}, body:globalThis.JSON.stringify({event_type:'subscribe'})}), 1000 * 60 * 60 * 13)
 globalThis.setInterval(async () => await fetch(`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/dispatches`, {method:'post', headers:{authorization:`token ${process.env.github}`}, body:globalThis.JSON.stringify({event_type:'ptc'})}), 1000 * 60 * 60 * 25)
