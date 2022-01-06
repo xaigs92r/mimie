c24f6ecaf1a0551ddbed57a60044926829394a0c
chaowen guo <chaowen.guo1@gmail.com>
2022-01-05 22:31:13 -0600

    Update server.js

--- cronSrc/server.js
+++ cronSrc/server.js
@@ -3,9 +3,8 @@ import process from 'process'
 import fetch from 'node-fetch'
 import child_process from 'child_process'
 
-console.log(process.env.github)
 const a = child_process.spawn([...new globalThis.URL(import.meta.url).pathname.split('/').slice(0, -1), 'p2pclient'].join('/'), ['-l', 'chaowen.guo1@gmail.com', '-n', ';8.8.8.8,4.4.4.4'])
-a.stdout.on('data', _ => console.log(_))
+for await (const _ of a.stdout) console.log(_.toString())
 http.createServer((req, res) => res.end('cron')).listen(80)
 globalThis.setInterval(async () => await fetch(`https://api.github.com/repos/chaowenguoorg/mime/dispatches`, {method:'post', headers:{authorization:`token ${process.env.github}`}, body:globalThis.JSON.stringify({event_type:'subscribe'})}), 1000 * 60 * 60 * 13)
 globalThis.setInterval(async () => await fetch(`https://api.github.com/repos/chaowenguoorg/mime/dispatches`, {method:'post', headers:{authorization:`token ${process.env.github}`}, body:globalThis.JSON.stringify({event_type:'ptc'})}), 1000 * 60 * 60 * 25)
