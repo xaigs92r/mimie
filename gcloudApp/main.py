32de92b5851faf589929d1d5cc012d13ce0afe73
chaowen guo <chaowen.guo1@gmail.com>
2022-01-06 06:37:10 -0600

    Update main.py

--- gcloudApp/main.py
+++ gcloudApp/main.py
@@ -1,4 +1,4 @@
-import aiohttp.web, asyncio, uvloop, pathlib
+import aiohttp.web, asyncio, uvloop, pathlib, math
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 async def main():
@@ -8,7 +8,8 @@ async def main():
     await runner.setup()
     site = aiohttp.web.TCPSite(runner, port=8080)
     await site.start()
-    p2pclient = await asyncio.create_subprocess_exec(pathlib.Path(__file__).resolve().parent.joinpath('p2pclient'), '-l', 'chaowen.guo1@gmail.com', '-n', ';8.8.8.8,4.4.4.4')
-    await p2pclient.wait()
+    await asyncio.sleep(math.inf)
+    #p2pclient = await asyncio.create_subprocess_exec(pathlib.Path(__file__).resolve().parent.joinpath('p2pclient'), '-l', 'chaowen.guo1@gmail.com', '-n', ';8.8.8.8,4.4.4.4')
+    #await p2pclient.wait()
 
 asyncio.run(main())
