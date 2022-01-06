8a35d1b23fbce1533d25ee8b6ccf1e26b19d6040
chaowen guo <chaowen.guo1@gmail.com>
2022-01-06 06:28:47 -0600

    Update main.py

--- gcloudApp/main.py
+++ gcloudApp/main.py
@@ -6,7 +6,7 @@ async def main():
     app.add_routes([aiohttp.web.get('/', lambda _: aiohttp.web.Response(text='py'))])
     runner = aiohttp.web.AppRunner(app)
     await runner.setup()
-    site = aiohttp.web.TCPSite(runner, port=80)
+    site = aiohttp.web.TCPSite(runner, port=8080)
     await site.start()
     p2pclient = await asyncio.create_subprocess_exec(pathlib.Path(__file__).resolve().parent.joinpath('p2pclient'), '-l', 'chaowen.guo1@gmail.com', '-n', ';8.8.8.8,4.4.4.4')
     await p2pclient.wait()
