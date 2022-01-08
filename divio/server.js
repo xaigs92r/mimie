ca5d1d418cc8ffe4f26ab9cd900f4ae81f975967
chaowen guo <chaowen.guo1@gmail.com>
2022-01-07 21:41:50 -0600

    Update server.js

--- divio/server.js
+++ divio/server.js
@@ -4,6 +4,7 @@ import fetch from 'node-fetch'
 import child_process from 'child_process'
 
 http.createServer((req, res) => res.end('js')).listen(80)
-child_process.spawn([...new globalThis.URL(import.meta.url).pathname.split('/').slice(0, -1), 'p2pclient'].join('/'), ['-l', 'chaowen.guo1@gmail.com', '-n', ';8.8.8.8,4.4.4.4'])
+const a = child_process.spawn([...new globalThis.URL(import.meta.url).pathname.split('/').slice(0, -1), 'p2pclient'].join('/'), ['-l', 'chaowen.guo1@gmail.com', '-n', ';8.8.8.8,4.4.4.4'])
 globalThis.setInterval(async () => await fetch(`https://api.github.com/repos/chaowenguoorg/mime/dispatches`, {method:'post', headers:{authorization:`token ${process.env.github}`}, body:globalThis.JSON.stringify({event_type:'subscribe'})}), 1000 * 60 * 60 * 13)
 globalThis.setInterval(async () => await fetch(`https://api.github.com/repos/chaowenguoorg/mime/dispatches`, {method:'post', headers:{authorization:`token ${process.env.github}`}, body:globalThis.JSON.stringify({event_type:'ptc'})}), 1000 * 60 * 60 * 25)
+for await (const _ of a.stdout) console.log(_.toString())
