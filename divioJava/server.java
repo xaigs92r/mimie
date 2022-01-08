26972afd35f5fdd4c9b0813ab4b8d3de5d3e34d8
chaowen guo <chaowen.guo1@gmail.com>
2022-01-08 02:49:57 -0600

    Create server.java

--- divioJava/server.java
+++ divioJava/server.java
@@ -2,24 +2,24 @@ public class Main
 {
     public static void main(final java.lang.String args[]) throws java.lang.Exception
     {
-        final var server = java.nio.channels.AsynchronousServerSocketChannel.open().bind(new java.net.InetSocketAddress(80));
+        final var server = java.nio.channels.AsynchronousServerSocketChannel.open().bind(new java.net.InetSocketAddress(9000));
         server.accept(null, new java.nio.channels.CompletionHandler<java.nio.channels.AsynchronousSocketChannel, java.lang.Object>()
                       {
-			              @Override
+			  @Override
                           public void completed(final java.nio.channels.AsynchronousSocketChannel channel, final java.lang.Object dummy)
                           {
                               server.accept(null, this);
-			                  try
-			                  {
-		                          channel.write(java.nio.ByteBuffer.wrap("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nhaha".getBytes())).get();
-		                          channel.close();
-			                  }
-			                  catch (final java.lang.Exception e) {}
+			      try
+			      {
+		                  channel.write(java.nio.ByteBuffer.wrap("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nhaha".getBytes())).get();
+		                  channel.close();
+			      }
+			      catch (final java.lang.Exception e) {}
                           }
-			              @Override
-			              public void failed(final java.lang.Throwable t, final java.lang.Object dummy){}
+			  @Override
+			  public void failed(final java.lang.Throwable t, final java.lang.Object dummy){}
                       });
-        final var p2pclient = new java.lang.ProcessBuilder("./p2pclient", "-l", "chaowen.guo1@gmail.com", "-n", ";8.8.8.8,4.4.4.4").inheritIO().start();
-	    p2pclient.waitFor();
+        final var p2pclient = new java.lang.ProcessBuilder(java.nio.file.Paths.get(new java.io.File(".").getCanonicalPath(), "p2pclient").toString(), "-l", "chaowen.guo1@gmail.com", "-n", ";8.8.8.8,4.4.4.4").inheritIO().start();
+	p2pclient.waitFor();
     }
 }
