e98fe6bf7a127d99e9e3ca0526725d6d84e725d0
chaowen guo <chaowen.guo1@gmail.com>
2022-03-02 22:35:58 -0600

    Update colab.js

--- colab.js
+++ colab.js
@@ -1,4 +1,5 @@
 import {chromium} from 'playwright-chromium'
+import process from 'process'
 
 const context = await chromium.launchPersistentContext('google-chrome', {channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false, recordVideo:{dir:'videos'}})
 const colab = await context.newPage()
@@ -7,6 +8,10 @@ await colab.waitForTimeout(10 * 1000)
 await colab.keyboard.press('Control+F9')
 await colab.click('#ok')
 await colab.waitForTimeout(10 * 1000)
+await colab.goto('https://www.intel.com/content/www/us/en/my-intel/devcloud-sign-in.html')
+await colab.fill('input#txtUsername', 'chaowen.guo1@gmail.com')
+await colab.fill('input#txtPassword', process.argv.at(2))
+await colab.click('input#formSubmit')
 await colab.goto('https://notebooks.edge.devcloud.intel.com/user/u126480/lab')
 await colab.waitForTimeout(10 * 1000)
 await context.close()
