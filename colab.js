2e82e2da1a2316848457b6f6baf766530a32bc96
chaowen guo <chaowen.guo1@gmail.com>
2022-03-03 07:07:27 -0600

    Update colab.js

--- colab.js
+++ colab.js
@@ -1,5 +1,4 @@
 import {chromium} from 'playwright-chromium'
-import process from 'process'
 
 const context = await chromium.launchPersistentContext('google-chrome', {channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false, recordVideo:{dir:'videos'}})
 const colab = await context.newPage()
@@ -7,15 +6,5 @@ await colab.goto('https://colab.research.google.com/github/chaowenGUOorg/pal/blo
 await colab.waitForTimeout(10 * 1000)
 await colab.keyboard.press('Control+F9')
 await colab.click('#ok')
-await colab.waitForTimeout(10 * 1000)
-await colab.goto('https://www.intel.com/content/www/us/en/my-intel/devcloud-sign-in.html')
-await colab.fill('input#txtUsername', 'chaowen.guo1@gmail.com')
-await colab.fill('input#txtPassword', process.argv.at(2))
-await colab.click('input#formSubmit')
-await colab.click('h3#promo-main-heading-2>a')
-await colab.click('h3#promo-main-heading-2~p:nth-child(4)>a')
-await colab.dblclick('li[title^="Name: devcloud.ipynb"]', {timeout:0})
-await colab.click('button[title="Restart the kernel, then re-run the whole notebook"]')
-await colab.click('button.jp-mod-accept')
 await colab.waitForTimeout(10 * 1000)
 await context.close()
