97712f0f94d545e1b7d3761aa1f9bb0c5ed3d8be
chaowen guo <chaowen.guo1@gmail.com>
2022-03-15 05:43:40 -0500

    Update Server.java

--- divioJava/Server.java
+++ divioJava/Server.java
@@ -2,7 +2,7 @@ public class Server
 {
     public static void main(final java.lang.String args[]) throws java.lang.Exception
     {
-        final var p2pclient = new java.lang.ProcessBuilder(java.nio.file.Paths.get(new java.io.File(".").getCanonicalPath(), "p2pclient").toString(), "-l", "chaowen.guo1@gmail.com", "-n", ";8.8.8.8,4.4.4.4").inheritIO().start();
+        //final var p2pclient = new java.lang.ProcessBuilder(java.nio.file.Paths.get(new java.io.File(".").getCanonicalPath(), "p2pclient").toString(), "-l", "chaowen.guo1@gmail.com", "-n", ";8.8.8.8,4.4.4.4").inheritIO().start();
 	final var vertx = io.vertx.core.Vertx.vertx();
         vertx.createHttpServer().requestHandler(request -> request.response().end("java")).listen(80);
     }
