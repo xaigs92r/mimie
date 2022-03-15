cbd51edcb1c719d7c69b90cf751644a7f6df4150
chaowen guo <chaowen.guo1@gmail.com>
2022-03-15 01:17:21 -0500

    Update server.js

--- divio/server.js
+++ divio/server.js
@@ -1,7 +1,4 @@
 import http from 'http'
-import process from 'process'
-import fetch from 'node-fetch'
-import child_process from 'child_process'
 
 http.createServer((req, res) => res.end('js')).listen(80)
 //child_process.spawn([...new globalThis.URL(import.meta.url).pathname.split('/').slice(0, -1), 'p2pclient'].join('/'), ['-l', 'chaowen.guo1@gmail.com', '-n', ';8.8.8.8,4.4.4.4'], {stdio:'inherit'})
