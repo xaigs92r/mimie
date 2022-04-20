bc0898cd4b1f74dd0cf85e49abb77801e4d138a3
chaowen guo <chaowen.guo1@gmail.com>
2022-04-19 19:59:48 -0500

    Update mime.js

--- mime.js
+++ mime.js
@@ -2,9 +2,9 @@ import {chromium} from 'playwright-chromium'
 import os from 'os'
 import process from 'process'
 
-const browser = await chromium.launch({channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false})
-globalThis.setTimeout(async () => await browser.close(), 1000 * 60 * 60 * 5.5)
-const context = await browser.newContext({recordVideo:{dir:'videos'}})
+const browser = await chromium.launch({channel:'chrome', args:['--disable-blink-features=AutomationControlled', '--start-maximized'], headless:false})
+globalThis.setTimeout(async () => await browser.close(), 1000 * 60 * 60)
+const context = await browser.newContext({recordVideo:{dir:'videos'}, viewport:null})
 const me = await context.newPage()
 const client = await context.newCDPSession(me)
 await client.send('Emulation.setScriptExecutionDisabled', {value:true})
@@ -26,14 +26,14 @@ await client.send('Emulation.setScriptExecutionDisabled', {value:false})
 const popup = await context.newPage()
 await popup.goto('http://cashmining.me/mining.php')
 //await me.evaluateHandle(cpus => globalThis.WMP.User(globalThis.document.querySelector('div#wmp-container').getAttribute('wmp-site-key'), globalThis.document.querySelector('div#wmp-container').getAttribute('wmp-username'), {threads:cpus,autoThreads:false,throttle:globalThis.document.querySelector('div#wmp-container').getAttribute('wmp-throttle'),forceASMJS:false}).start(), os.cpus().length)
-const it = await browser.newPage({recordVideo:{dir:'videos'}})
+const it = await browser.newPage({recordVideo:{dir:'videos'}, viewport:null})
 await it.goto('https://cashmining.forumforyou.it/')
 await it.click('a.nav-link')
 await it.fill('input[name="user"]', 'chaowen.guo1@gmail.com')
 await it.fill('input[name="password"]', process.argv.at(2))
 await it.click('button[name="connect"]')
 await globalThis.Promise.all([it.waitForEvent('popup'), it.click('button[onclick]')])
-const adfreeway = await context.newPage({recordVideo:{dir:'videos'}})
+const adfreeway = await context.newPage({recordVideo:{dir:'videos'}, viewport:null})
 await adfreeway.goto('https://adfreeway.com/users/sign_in')
 await adfreeway.fill('input#user_email', 'chaowen.guo1@gmail.com')
 await adfreeway.fill('input#user_password', process.argv.at(2))
@@ -42,7 +42,18 @@ for (;;)
 {
     await adfreeway.waitForTimeout(1000)
     const onviewport = adfreeway.locator('div.fp.onviewport')
-    const like = onviewport.locator(':scope input[alt="Like ugc"]')
+    try
+    {
+        await onviewport.first().locator(':scope>div.ugc-wrap').last().scrollIntoViewIfNeeded()
+    }
+    catch
+    {
+        await adfreeway.screenshot({path:'screenshot.png'})
+        await browser.close()
+        process.exit(0)
+    }
+    await adfreeway.waitForTimeout(1000)
+    const like = onviewport.first().locator(':scope input[alt="Like ugc"]')
     for (const _ of globalThis.Array(await like.count()).keys())
     {
         await like.nth(_).click()
