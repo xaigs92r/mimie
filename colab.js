bbc1226e316aacddb5c84a750df8a3b5f4505165
0chaowenguo <104413753+0chaowenguo@users.noreply.github.com>
2022-05-02 03:44:52 -0500

    Update colab.js

--- colab.js
+++ colab.js
@@ -1,6 +1,6 @@
 import {chromium} from 'playwright-chromium'
 
-const context = await chromium.launchPersistentContext('google-chrome', {channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false, recordVideo:{dir:'videos'}})
+const context = await chromium.launchPersistentContext('google-chrome', {channel:'chrome', args:['--disable-blink-features=AutomationControlled', '--start-maximized'], headless:false, recordVideo:{dir:'videos'}, viewport:null})
 const colab = await context.newPage()
 await colab.goto('https://colab.research.google.com/github/chaowenGUOorg/pal/blob/main/colab.ipynb')
 await colab.waitForTimeout(10 * 1000)
