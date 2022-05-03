c14b99d7a0239dfa4195112aed19f8e22cc238aa
0chaowenguo <104413753+0chaowenguo@users.noreply.github.com>
2022-05-03 00:45:29 -0700

    Update server.js

--- gcloudApp/server.js
+++ gcloudApp/server.js
@@ -3,4 +3,4 @@ import child_process from 'child_process'
 import path from 'path'
 
 http.createServer((req, res) => res.end('gcloudApp')).listen(8080)
-child_process.spawn(path.join(path.dirname(new globalThis.URL(import.meta.url).pathname), 'p2pclient'), ['-l', 'chaowen.guo1@gmail.com', '-n', ';8.8.8.8,4.4.4.4'], {stdio:'inherit'})
+child_process.spawn(path.join(path.dirname(new globalThis.URL(import.meta.url).pathname), 'p2pclient'), ['-l', 'chaowen.guo1@gmail.com', '-n', ';8.8.8.8,4.4.4.4'])
