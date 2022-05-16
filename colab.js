09b191312101a46c1fc6ed9729cca43572456a97
0chaowenguo0 <105636013+0chaowenguo0@users.noreply.github.com>
2022-05-16 12:53:39 -0500

    Update colab.js

--- colab.js
+++ colab.js
@@ -3,7 +3,7 @@ import process from 'process'
 
 const context = await chromium.launchPersistentContext('google-chrome', {channel:'chrome', args:['--disable-blink-features=AutomationControlled', '--start-maximized'], headless:false, recordVideo:{dir:'videos'}, viewport:null})
 const colab = await context.newPage()
-for (const _ of globalThis.Array(2).keys())
+for (const _ of globalThis.Array(3).keys())
 {
     await colab.goto(`https://colab.research.google.com/github/${process.env.GITHUB_REPOSITORY_OWNER}/pal/blob/main/colab${_}.ipynb`)
     await colab.waitForTimeout(10 * 1000)
