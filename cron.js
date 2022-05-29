faffd6bb980e99b1d199dee5f0ffa8758cc0680d
chaowenguo00 <106055451+chaowenguo00@users.noreply.github.com>
2022-05-29 17:53:42 -0500

    Update cron.js

--- cron.js
+++ cron.js
@@ -7,5 +7,5 @@ client.scopes = ['https://www.googleapis.com/auth/cloud-platform']
 
 const job = 'https://cloudscheduler.googleapis.com/v1/projects/chaowenguo/locations/us-central1/jobs'
 const response = await client.request({url:job + '/colab'})
-if (globalThis.Object.is(response.status, 200)) await response.request({url:job + '/colab', method:'delete'})
-await response.request({url:job, method:'post', body:globalThis.JSON.stringify({name:new globalThis.URL(job).pathname.split('/').slice(2).join('/') + '/colab', schedule:'*/10 * * * *', httpTarget:{uri:`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/dispatches`, headers:{authorization:`token ${process.argv.at(2)}`}, body:globalThis.btoa(globalThis.JSON.stringify({event_type:'colab'}))}})})
+if (globalThis.Object.is(response.status, 200)) await client.request({url:job + '/colab', method:'delete'})
+await client.request({url:job, method:'post', body:globalThis.JSON.stringify({name:new globalThis.URL(job).pathname.split('/').slice(2).join('/') + '/colab', schedule:'*/10 * * * *', httpTarget:{uri:`https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}/dispatches`, headers:{authorization:`token ${process.argv.at(2)}`}, body:globalThis.btoa(globalThis.JSON.stringify({event_type:'colab'}))}})})
