cb066a6d5196ffe76fd017715a231dd0f8259913
chaowen guo <chaowen.guo0@gmail.com>
2022-06-10 05:44:14 +0000

    colab.js edited online with Bitbucket

--- colab.js
+++ colab.js
@@ -3,9 +3,9 @@ import process from 'process'
 
 const context = await chromium.launchPersistentContext('google-chrome', {channel:'chrome', args:['--disable-blink-features=AutomationControlled', '--start-maximized'], headless:false, recordVideo:{dir:'videos'}, viewport:null})
 const colab = await context.newPage()
-for (const _ of globalThis.Array(4).keys())
+for (const _ of ['1NG6jXbYo09JWIPDD6JpQL9XphdSbZ0B1'])
 {
-    await colab.goto(`https://colab.research.google.com/github/${process.env.GITHUB_REPOSITORY_OWNER}/pal/blob/main/colab${_}.ipynb`)
+    await colab.goto(`https://colab.research.google.com/drive/${_}`)
     await colab.waitForTimeout(10 * 1000)
     await colab.keyboard.press('Control+F9')
     await colab.click('#ok')
