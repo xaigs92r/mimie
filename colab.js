49218a53afa2daeaf61b40a5ab2f6ac62d3ceb67
bvttvb <107609050+bvttvb@users.noreply.github.com>
2022-06-16 20:59:20 -0500

    Update colab.js

--- colab.js
+++ colab.js
@@ -3,12 +3,11 @@ import process from 'process'
 
 const context = await chromium.launchPersistentContext('google-chrome', {channel:'chrome', args:['--disable-blink-features=AutomationControlled', '--start-maximized'], headless:false, recordVideo:{dir:'videos'}, viewport:null})
 const colab = await context.newPage()
-for (const _ of ['1NG6jXbYo09JWIPDD6JpQL9XphdSbZ0B1', '1VLoQqDqeSPisGX14UBy9v3dXUtRHwrRp', '1hE2wkzqto0Y-ZmLeI5Aa5TVRd3nsrlY_', '1vWOfoofdVF4ysWGOokqPr4LziGfK2A0O'])
+for (const _ of ['1NG6jXbYo09JWIPDD6JpQL9XphdSbZ0B1', '1VLoQqDqeSPisGX14UBy9v3dXUtRHwrRp', '1hE2wkzqto0Y-ZmLeI5Aa5TVRd3nsrlY_'])
 {
     await colab.goto(`https://colab.research.google.com/drive/${_}`)
     await colab.waitForTimeout(10 * 1000)
     await colab.keyboard.press('Control+F9')
-    await colab.click('#ok')
     await colab.waitForTimeout(10 * 1000)
 }
 await context.close()
