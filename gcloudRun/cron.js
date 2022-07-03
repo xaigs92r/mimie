077cae707a0af30325ab01f9d80e692dad36ffa5
chaowen guo <chaowen.guo0@gmail.com>
2022-07-03 09:50:14 +0000

    cron.js created online with Bitbucket

---
+++ gcloudRun/cron.js
@@ -0,0 +1,9 @@
+import {promises as fs} from 'fs'
+import {auth} from 'google-auth-library'
+import process from 'process'
+
+const client = auth.fromJSON(globalThis.JSON.parse(await fs.readFile('gcloud')))
+client.scopes = ['https://www.googleapis.com/auth/cloud-platform']
+
+const job = 'https://cloudscheduler.googleapis.com/v1/projects/chaowenguo/locations/us-central1/jobs'
+await client.request({url:job, method:'post', body:globalThis.JSON.stringify({name:new globalThis.URL(job).pathname.split('/').slice(2).join('/') + '/build', schedule:'0 0 * * *', httpTarget:{uri:'https://us-central1-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/chaowenguo/jobs/build:run', oauthToken:{serviceAccountEmail:'chaowenguo@chaowenguo.iam.gserviceaccount.com'}}})})
