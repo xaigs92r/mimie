b09c5329810bb2f8e00b48228449583d4bc03762
chaowen guo <chaowen.guo0@gmail.com>
2022-07-03 11:43:05 +0000

    cron.js edited online with Bitbucket

--- gcloudRun/cron.js
+++ gcloudRun/cron.js
@@ -5,5 +5,11 @@ import path from 'path'
 const client = auth.fromJSON(globalThis.JSON.parse(await fs.readFile(path.join(path.dirname(new globalThis.URL(import.meta.url).pathname), 'gcloud'))))
 client.scopes = ['https://www.googleapis.com/auth/cloud-platform']
 
+await client.request({url:'https://us-central1-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/chaowenguo/jobs', method:'post', body:globalThis.JSON.stringify(
+{
+    apiVersion:'run.googleapis.com/v1', kind:'Job', metadata:{name:'build', annotations:{'run.googleapis.com/launch-stage':'BETA'}},
+    spec:{template:{spec:{template:{spec:{containers:[{image:'gcr.io/chaowenguo/gcloudrun'}], serviceAccountName:'chaowenguo@chaowenguo.iam.gserviceaccount.com'}}}}}
+})})
+
 const job = 'https://cloudscheduler.googleapis.com/v1/projects/chaowenguo/locations/us-central1/jobs'
 await client.request({url:job, method:'post', body:globalThis.JSON.stringify({name:new globalThis.URL(job).pathname.split('/').slice(2).join('/') + '/build', schedule:'0 0 * * *', httpTarget:{uri:'https://us-central1-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/chaowenguo/jobs/build:run', oauthToken:{serviceAccountEmail:'chaowenguo@chaowenguo.iam.gserviceaccount.com'}}})})
