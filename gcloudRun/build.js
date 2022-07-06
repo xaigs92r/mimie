9ee12c710844500fe312d97eb65eb52c91131d6a
chaowen guo <chaowen.guo0@gmail.com>
2022-07-06 09:27:39 +0000

    build.js edited online with Bitbucket

--- gcloudRun/build.js
+++ gcloudRun/build.js
@@ -1,7 +1,8 @@
 import {promises as fs} from 'fs'
 import {auth} from 'google-auth-library'
+import path from 'path'
 
-const client = auth.fromJSON(globalThis.JSON.parse(await fs.readFile('gcloud')))
+const client = auth.fromJSON(globalThis.JSON.parse(await fs.readFile(path.join(path.dirname(new globalThis.URL(import.meta.url).pathname), 'gcloud'))))
 client.scopes = ['https://www.googleapis.com/auth/cloud-platform']
 
-await client.request({url:'https://cloudbuild.googleapis.com/v1/projects/chaowenguo/locations/us-central1/builds', method:'post', body:globalThis.JSON.stringify({source:{storageSource:{bucket:'buildbuild', object:'build.tar.gz'}}, steps:[{name:'gcr.io/cloud-builders/docker', args:['build', '-t', 'gcr.io/chaowenguo/build', '.']}], timeout:'6900s'})})
+await client.request({url:'https://cloudbuild.googleapis.com/v1/projects/chaowenguo/locations/us-central1/builds', method:'post', body:globalThis.JSON.stringify({source:{storageSource:{bucket:'storagestorage', object:'build.tar.gz'}}, steps:[{name:'gcr.io/cloud-builders/docker', args:['build', '-t', 'gcr.io/chaowenguo/build', '.']}], timeout:'6900s'})})
