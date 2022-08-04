b524f3bafba030f79be82fb1748d233ff66012a2
r1nny <109706512+r1nny@users.noreply.github.com>
2022-08-04 18:00:23 -0500

    Update colab.js

--- colab.js
+++ colab.js
@@ -8,6 +8,6 @@ for (const _ of ['1MmQ8rxXD0qWnW57Q0PEMrZL68Qm9heBg', '1nyUYBHm63g-VjG0xv-EEBDZo
     await colab.goto(`https://colab.research.google.com/drive/${_}`)
     await colab.waitForTimeout(10 * 1000)
     await colab.keyboard.press('Control+F9')
-    await colab.waitForTimeout(10 * 1000)
+    await colab.waitForTimeout(30 * 1000)
 }
 await context.close()
