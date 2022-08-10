ba4cfc3a7892a3597f6d28131ed9894355ecc7e8
r1nny <109706512+r1nny@users.noreply.github.com>
2022-08-09 20:01:06 -0500

    Update colab.js

--- colab.js
+++ colab.js
@@ -2,10 +2,10 @@ import {chromium} from 'playwright-chromium'
 import process from 'process'
 
 const context = await chromium.launchPersistentContext('google-chrome', {channel:'chrome', args:['--disable-blink-features=AutomationControlled', '--start-maximized'], headless:false, recordVideo:{dir:'videos'}, viewport:null})
-const colab = await context.newPage()
 
 async function f(_)
 {
+    const colab = await context.newPage()
     await colab.goto(`https://colab.research.google.com/drive/${_}`)
     await colab.waitForTimeout(10 * 1000)
     await colab.keyboard.press('Control+F9')
