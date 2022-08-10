126acd0f0beec52bdc01a646c0299c5a234ae993
r1nny <109706512+r1nny@users.noreply.github.com>
2022-08-09 19:40:48 -0500

    Update colab.js

--- colab.js
+++ colab.js
@@ -3,11 +3,14 @@ import process from 'process'
 
 const context = await chromium.launchPersistentContext('google-chrome', {channel:'chrome', args:['--disable-blink-features=AutomationControlled', '--start-maximized'], headless:false, recordVideo:{dir:'videos'}, viewport:null})
 const colab = await context.newPage()
-for (const _ of ['1MmQ8rxXD0qWnW57Q0PEMrZL68Qm9heBg', '1nyUYBHm63g-VjG0xv-EEBDZoTlPcx6ze', '1oP6Nb201lv0tzW_oaV_tHcdSstpby-5L'])
+
+async function f(_)
 {
     await colab.goto(`https://colab.research.google.com/drive/${_}`)
     await colab.waitForTimeout(10 * 1000)
     await colab.keyboard.press('Control+F9')
     await colab.waitForTimeout(45 * 1000)
 }
+
+await globalThis.Promise.all(['1MmQ8rxXD0qWnW57Q0PEMrZL68Qm9heBg', '1nyUYBHm63g-VjG0xv-EEBDZoTlPcx6ze', '1oP6Nb201lv0tzW_oaV_tHcdSstpby-5L'].map(f))
 await context.close()
