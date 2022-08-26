6658beb314c0bdff04e2b14b2d1aae1c55f7ec0c
r1nny <109706512+r1nny@users.noreply.github.com>
2022-08-26 03:52:05 -0500

    Delete watch.js

--- watch.js
+++
@@ -1,9 +0,0 @@
-import {chromium} from 'playwright-chromium'
-
-const browser = await chromium.launch({channel:'chrome', args:['--disable-blink-features=AutomationControlled'], headless:false})
-const youtube = await browser.newPage({recordVideo:{dir:'videos'}})
-await youtube.goto('https://www.youtube.com/watch?v=epHjCti0dIs')
-const moviePlayer = await youtube.$('div#movie_player')
-await moviePlayer.evaluateHandle(_ => _.style.display = 'block')
-await moviePlayer.waitForElementState('visible')
-await moviePlayer.evaluateHandle(_ => _.playVideo())
